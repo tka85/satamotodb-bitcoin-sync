c6b69870a78f4bd1727ba52b0559d0658e3b9439
tak85 <thaliskalf@gmail.com>
2020-04-20 15:50:40 +0200

    bitcoind-start.sh script checks config file exists
    In case /opt/bitcoin/.bitcoin/bitcoind.conf is not there
    (accidentaly or on purpose not mounted in docker-compose)
    starting bitcoind is skipped but containers still stay running.

--- bitcoind-start.sh
+++ bitcoind-start.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/bin/su - bitcoin -c "/usr/local/bin/bitcoind -datadir=/opt/bitcoin/.bitcoin -conf=bitcoin.conf"
+if [ -r /opt/bitcoin/.bitcoin/bitcoin.conf ]; then
+    /bin/su - bitcoin -c "/usr/local/bin/bitcoind -datadir=/opt/bitcoin/.bitcoin -conf=bitcoin.conf"
+fi
+# Block script from terminating in case the bitcoin.conf is not there yet
+tail -f /dev/null;
